f9d09e0786b25ea5d7ad604664b301a32696ae3e
gwarner3 <george@georgedevelops.com>
2017-07-19 16:51:23 -0500

    coded getRolledNumber up to capturing the number of die sides

--- dicegame.js
+++ dicegame.js
@@ -1,37 +1,38 @@
 "use strict"
 function diceGameStart() {
 	var dieArray = [4,6,8,12,20];
-	var currentDie = dieArray[1];
+	var dieSides = 1;
 	var currentPosition = 0;
 	var mountainTop = 60;
 	
 	//While the user is in position from 0 to mountainTop. //while(currentPosition > 0 && currentPosition <= mountainTop)
 	while(currentPosition >= 0 && currentPosition <= mountainTop) {
 
-		//display users current game status. Since game is first starting, currentPosition = 0, dieArray[currentDie]
-		console.log("You are at position " + currentPosition + " and you have a " + currentDie + " sided die." );
+		//display users current game status. Since game is first starting, currentPosition = 0, dieArray[dieSides]
+		console.log("You are at position " + currentPosition + " and you have a " + dieSides + " sided die." );
 		//test code currentPosition++; 
 		
 		//prompt user to roll die
-		var rollDie = confirm("Roll your die. OK to roll, Cancel to quit game.");
+		var rollDie = confirm("Roll your die. Click OK to roll die, Cancel to quit game.");
 			//If user clicks OK
-				//declare numberRolled = getNumberRolled(dieArray[currentDie])
+				//declare numberRolled = getNumberRolled(dieArray[dieSides])
 				//will return the numberRolled
 			if (rollDie) {
-				var numberRolled = getNumberRolled(dieArray[currentDie]);			
+				var numberRolled = getNumberRolled(dieArray[dieSides]);
+				;break //testing purposes only			
 			} else {
-				//code to run if user cliskc cancel button. For now, break app
-				break;
+				//code to run if user clicks cancel button. For now, break app
+				break; //testing purposes only
 			}
-			
+
 		//move user based on number rolled.
 		//declare newPosition = moveUser(numberRolled, currentPosition)
 	
 	}//END WHILE
 }
-function getNumberRolled() {//parameter is dieArray[currentDie]
+function getNumberRolled(dieSides) {//parameter is die index in dieArray[dieSides]
 	//declare randomNumber as number generated based on what die was rolled. i.e, if die 4 is being rolled, random number between 1 and 4
-	
+	var x = dieSides;	
 	//return random number rolled
 }
 function moveUser() {//determines how/where to move user. Parameters: numberRolled, currentPosition 
