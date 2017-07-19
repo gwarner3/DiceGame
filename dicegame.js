a2b577b129bc60c77ca412e2307d3a705c8add05
gwarner3 <george@georgedevelops.com>
2017-07-19 16:20:29 -0500

    coded diceGameStart up to getNumberRolled algorithm

--- dicegame.js
+++ dicegame.js
@@ -1,22 +1,33 @@
 "use strict"
 function diceGameStart() {
-	//declare dieArray[4,6,8,12,20]
-	//declare currentDie = dieArray[1]
-	//declare currentPosition = 0
-	//declare mountainTop = 60
+	var dieArray = [4,6,8,12,20];
+	var currentDie = dieArray[1];
+	var currentPosition = 0;
+	var mountainTop = 60;
 	
 	//While the user is in position from 0 to mountainTop. //while(currentPosition > 0 && currentPosition <= mountainTop)
+	while(currentPosition >= 0 && currentPosition <= mountainTop) {
 
-		//display users current game status. Since game is first starting, currentPosition = 0, dieArray[currentDie] 
+		//display users current game status. Since game is first starting, currentPosition = 0, dieArray[currentDie]
+		console.log("You are at position " + currentPosition + " and you have a " + currentDie + " sided die." );
+		//test code currentPosition++; 
 		
 		//prompt user to roll die
-			//declare numberRolled = getNumberRolled(dieArray[currentDie])
-			//will return the numberRolled
-
+		var rollDie = confirm("Roll your die. OK to roll, Cancel to quit game.");
+			//If user clicks OK
+				//declare numberRolled = getNumberRolled(dieArray[currentDie])
+				//will return the numberRolled
+			if (rollDie) {
+				var numberRolled = getNumberRolled(dieArray[currentDie]);			
+			} else {
+				//code to run if user cliskc cancel button. For now, break app
+				break;
+			}
+			
 		//move user based on number rolled.
 		//declare newPosition = moveUser(numberRolled, currentPosition)
 	
-	//END WHILE
+	}//END WHILE
 }
 function getNumberRolled() {//parameter is dieArray[currentDie]
 	//declare randomNumber as number generated based on what die was rolled. i.e, if die 4 is being rolled, random number between 1 and 4
@@ -38,29 +49,3 @@ function moveUser() {//determines how/where to move user. Parameters: numberRoll
 	//returns the users new position
 }
 diceGameStart();//Starts aplication
-
-
-
-
-
-//Mountain climber
-//1 player (FE: use 3 players)
-//A mountain is 60 spaces high (FE: allow player to select mountain height)
-//player starts with a 6 sided dice
-//player starts at position 0, at the bottom of the mountain
-//player  rolls a die
-	//If player rolls an odd number, and they have not moved forward at all, they do not move at all
-	//If player rolls an odd number and have moved forward, they move backward the number of spaces rolled
-		//If the number of spaces they are to move backward is greater than the number of spaces they have moved forward, they fall off the mountain and are out of the game
-	//If player rolls an even number, they move forward the number of spaces rolled
-	//If a player rolls a 1, they move forward 1 and they upgrade to the next die.
-		//If player die already has 20 faces, user does not upgrade or downgrade die
-	//If a player rolls a 6, they downgrade one die
-//Fame ends when/if user moves 60 or more spaces
-
-// Design and code a game that uses at least one 4-sided, 6-sided, 8-sided, 10-sided, 12-sided, and 20-sided dice.
-// -    You come up with the rules and design
-// -    The game must involve some form of user interaction (user “rolls” the dice perhaps)
-// -    The rules must be more complicated than the game War. This means the game rules can’t be as simple as the person with the greater dice value wins. That can be part of the rules, but the game needs to be more complex than that.
-// -    You can either create a console application (console.log) or interact with HTML/CSS elements for your game (.innerHTML)
-// -    HINT: Make sure you reach the Minimal Viable Product (MVP) before doing anything super fancy. Get it working and then make it better.
