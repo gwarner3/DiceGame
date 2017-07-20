b481333d4c27e0fbc3da5d0fe775e4a109d55212
gwarner3 <george@georgedevelops.com>
2017-07-20 14:44:40 -0500

    removed remaining comments except in displayGameStatus function. Succesfully tested all scenarios

--- dicegame.js
+++ dicegame.js
@@ -5,21 +5,15 @@ function diceGameStart() {
 	var currentPosition = 0;
 	var mountainTop = 10;
 	
-	//display users starting game status
 	displayGameStatus(currentPosition, numberRolled, dieArray[dieSides], mountainTop);
 
-	//While the user is in position from 0 to mountainTop. //while(currentPosition > 0 && currentPosition <= mountainTop)
 	while(currentPosition >= 0 && currentPosition < mountainTop) {
-		//prompt user to roll die
 		var rollDie = confirm("Roll your die. Click OK to roll die, Cancel to quit game.");
-			//If user clicks OK
-				//declare numberRolled = getNumberRolled(dieArray[dieSides])
-				//will return the numberRolled
+
 			if (rollDie) {
 				var numberRolled = getNumberRolled(dieArray[dieSides]);
 			} else {
-				//code to run if user clicks cancel button. For now, break app
-				break; //testing purposes only
+				break;
 			}
 
 		currentPosition = moveUser(numberRolled, currentPosition);
@@ -41,7 +35,7 @@ function upgradeDie(numberRolled, dieSides) {
 function displayGameStatus(currentPosition, numberRolled, dieSides, mountainTop) {
 	if (currentPosition >= mountainTop){		
 		console.log("You won");		
-	} else if (currentPosition === 0 && numberRolled < 0) {//if player currentPosition = 0 and numberRolled < 0		
+	} else if (currentPosition === 0 && numberRolled < 0) {
 		//do nothing
 	} else if (currentPosition < 0) {//if player numberRolled > currentPosition and currentPosition > 0		
 		//player has fallen off the mountain
@@ -51,26 +45,19 @@ function displayGameStatus(currentPosition, numberRolled, dieSides, mountainTop)
 		console.log("You are at position " + currentPosition + " and you have a " + dieSides + " sided die." );
 	}	
 }
-function getNumberRolled(dieSides) {//parameter is die index in dieArray[dieSides]
-	//declare randomNumber as number generated based on what die was rolled. i.e, if die 4 is being rolled, random number between 1 and 4
+function getNumberRolled(dieSides) {
 	var numberRolled = Math.floor((Math.random() * dieSides) + 1);
 	return numberRolled;		
-	//return random number rolled
 }
-function moveUser(numberRolled, currentPosition) {//determines how/where to move user. Parameters: numberRolled, currentPosition 
+function moveUser(numberRolled, currentPosition) { 
 	var newPosition;
 	if (numberRolled % 2 !== 0 && currentPosition === 0) {
-		//numberRolled is odd
-		//If player rolls an odd number, and they have not moved forward at all, they do not move at all
 		newPosition = currentPosition;
 	} else if (numberRolled % 2 !== 0 && currentPosition > 0) {
-		//If player rolls an odd number and have moved forward, they move backward the number of spaces rolled
 		newPosition = currentPosition - numberRolled;
 	} else if (numberRolled % 2 === 0) {	
-	//numberRolled is even
-	//If player rolls an even number, they move forward the number of spaces rolled	
 		newPosition = numberRolled + currentPosition;
 	}
 	return newPosition;
 }
-diceGameStart();//Starts aplication
+diceGameStart();
