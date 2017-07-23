aed0399a3835c626d9a54ba9355aa18f64358b68
gwarner3 <george@georgedevelops.com>
2017-07-23 11:23:39 -0500

    Updated onjects to include boolean value isOnWall and set to true. Updated functions to not run if vaue is false for player

--- dicegame.js
+++ dicegame.js
@@ -5,7 +5,7 @@ function diceGameStart() {
 
 	alert("Quick Rules:\nYou 3 are free climbing a wall; no harness, no rope.\nRolling even numbers move you forward\nRolling odd numbers moves you backwards.\nRoll a 6 and you'll get a better die.\nRoll a 1 and you'll get a worse die.\nGood luck!");
 
-	while (players[0].isInGame && players[1].isInGame  && players[2].isInGame) {
+	while (players[0].isInGame && players[1].isInGame && players[2].isInGame) {
 		var rollDie = confirm("Roll the dice.");
 
 		 if (rollDie) {
@@ -18,22 +18,35 @@ function diceGameStart() {
 
 		upgradeDie(players);
 
-		//control tool to break out of while loop during tests 
-		players[1].isInGame = false;
-
+		displayGameStatus(players);
 	}
+}
+function displayGameStatus(players) {
+	for (var i = 0; i < players.length; i++) {
+		if (players[i].currentPosition >= players[i].mountainTop) {		
+			console.log("Congratulations " + players[i].playerName + ", you just rolled a " + players[i].numberRolled + " and free climbed a total " + players[i].currentPosition + " meters!");
+			players[i].isInGame	= false;
+		} else if (players[i].currentPosition < 0 && players[i].isOnWall === true) {
+			console.log("Uh-oh, " + players[i].playerName + " rolled a " + players[i].numberRolled + " and fell right off the wall.");
+			players[i].isOnWall = false;
+			players.splice[i];
+
+		} else {
+			console.log(players[i].playerName + " rolled a " + players[i].numberRolled + " and is at position " + players[i].currentPosition + " holding a " + players[i].dieArray[players[i].dieSides] + " sided die." );
+		}
+	}	
 }
 function upgradeDie(players) {
 	for (var i = 0; i < players.length; i++) {
 		switch(players[i].numberRolled) {
 			case 6:
-				if (players[i].dieSides !== 0) {
+				if (players[i].dieSides !== 0 && players[i].isOnWall === true) {
 					players[i].dieSides -=1 ;
 					console.log(players[i].playerName + " rolled 6 and will downgrade to a " + players[i].dieArray[players[i].dieSides] + " sided die");
 				}
 				break;			
 			case 1:
-				if (players[i].dieSides !==4) {
+				if (players[i].dieSides !==4 && players[i].isOnWall === true) {
 					players[i].dieSides += 1;
 					console.log(players[i].playerName + " rolled 1 and will upgrade to a " + players[i].dieArray[players[i].dieSides] + " sided die");
 				}
@@ -49,15 +62,17 @@ function moveUser(players) {
 			players[i].currentPosition = players[i].currentPosition;
 		} else if (players[i].numberRolled % 2 !== 0 && players[i].currentPosition > 0) {
 			players[i].currentPosition -= players[i].numberRolled;
-		} else if (players[i].numberRolled % 2 === 0) {	
+		} else if (players[i].numberRolled % 2 === 0 && players[i].isOnWall === true) {	
 			players[i].currentPosition += players[i].numberRolled;
 		}
 	}
 }
-function getNumberRolled(players) {	
-	for (var i = 0; i < players.length; i++) {
-		var numberRolled = Math.floor((Math.random() * players[i].dieArray[players[i].dieSides]) + 1);
-		players[i].numberRolled = numberRolled;
+function getNumberRolled(players) {
+	if (players[i].isOnWall) {
+		for (var i = 0; i < players.length; i++) {
+			var numberRolled = Math.floor((Math.random() * players[i].dieArray[players[i].dieSides]) + 1);
+			players[i].numberRolled = numberRolled;
+		}
 	}
 }
 function createPlayers() {
@@ -67,8 +82,9 @@ function createPlayers() {
 			dieArray: [4,6,8,12,20],
 			dieSides: 1,
 			currentPosition: 0,
-			mountainTop: 10,
+			mountainTop: 60,
 			isInGame: true,
+			isOnWall: true,
 			numberRolled: 0,
 		},
 		{
@@ -76,8 +92,9 @@ function createPlayers() {
 			dieArray: [4,6,8,12,20],
 			dieSides: 1,
 			currentPosition: 0,
-			mountainTop: 10,
+			mountainTop: 60,
 			isInGame: true,
+			isOnWall: true,
 			numberRolled: 0,
 		},
 		{
@@ -85,8 +102,9 @@ function createPlayers() {
 			dieArray: [4,6,8,12,20],
 			dieSides: 1,
 			currentPosition: 0,
-			mountainTop: 10,
+			mountainTop: 60,
 			isInGame: true,
+			isOnWall: true,
 			numberRolled: 0,
 		}
 	];
