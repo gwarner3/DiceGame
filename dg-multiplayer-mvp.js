2581f55651400e0b2c65720e8004c44fea9d5d5d
gwarner3 <george@georgedevelops.com>
2017-07-24 07:14:05 -0500

    Updated intro screen to indicate how game is won. Changed file names to indicate multi vs single player versiono and MVP files.

--- dg-multiplayer-mvp.js
+++ dg-multiplayer-mvp.js
@@ -3,7 +3,7 @@ function diceGameStart() {
 
 	var players = createPlayers();
 
-	alert("Quick Rules:\nYou 3 are free climbing a wall; no harness, no rope.\nRolling even numbers move you forward\nRolling odd numbers moves you backwards.\nRoll a 6 and you'll get a better die.\nRoll a 1 and you'll get a worse die.\nGood luck!");
+	alert("Quick Rules:\nYou 3 are free climbing a wall; no harness, no rope.\nRolling even numbers move you forward\nRolling odd numbers moves you backwards.\nRoll a 6 and you'll get a better die.\nRoll a 1 and you'll get a worse die.\nClimb the wall and you are a winner.\nGood luck!");
 
 	while (players[0].isInGame && players[1].isInGame && players[2].isInGame) {
 		var rollDie = confirm("Roll the dice.");
